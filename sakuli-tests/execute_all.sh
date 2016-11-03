91c49fab7ea1a24ef66c332e792cd1da81f4c0e9
Tobias Schneck <tobias.schneck@consol.de>
2016-11-03 09:41:15 +0100

    update sakuli-image versions

--- sakuli-tests/execute_all.sh
+++ sakuli-tests/execute_all.sh
@@ -6,8 +6,8 @@ FOLDER=$(pwd)
 TESTSUITE='blueberry' $FOLDER/execute_compose_test.sh $@ &
 #TESTSUITE='blueberry' BROWSER='firefox' $FOLDER/execute_compose_test.sh $@ &
 TESTSUITE='caramel' $FOLDER/execute_compose_test.sh $@ &
-TESTSUITE='chocolate' $FOLDER/execute_compose_test.sh $@ &
+TESTSUITE='chocolate' TEST_OS=centos $FOLDER/execute_compose_test.sh $@ &
 #TESTSUITE='chocolate' BROWSER='firefox' $FOLDER/execute_compose_test.sh $@ &
-TESTSUITE='order-pdf' $FOLDER/execute_compose_test.sh $@ &
+TESTSUITE='order-pdf' TEST_OS=centos $FOLDER/execute_compose_test.sh $@ &
 wait
 exit $?
