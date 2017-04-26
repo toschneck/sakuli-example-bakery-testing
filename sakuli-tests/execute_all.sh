68274ebd6ffe3d026888837a33a9b7b17973f498
Tobias Schneck <tobias.schneck@consol.de>
2017-04-26 12:43:21 +0200

    try to update to v1.1.0

--- sakuli-tests/execute_all.sh
+++ sakuli-tests/execute_all.sh
@@ -3,10 +3,10 @@ cd $(dirname `which $0`)
 FOLDER=$(pwd)
 
 ### does the same as the jenkins job
-TESTSUITE='blueberry' $FOLDER/execute_compose_test.sh $@ &
+#TESTSUITE='blueberry' $FOLDER/execute_compose_test.sh $@ &
 #TESTSUITE='blueberry' BROWSER='firefox' $FOLDER/execute_compose_test.sh $@ &
-TESTSUITE='caramel' $FOLDER/execute_compose_test.sh $@ &
-TESTSUITE='chocolate' $FOLDER/execute_compose_test.sh $@ &
+#TESTSUITE='caramel' $FOLDER/execute_compose_test.sh $@ &
+#TESTSUITE='chocolate' $FOLDER/execute_compose_test.sh $@ &
 #TESTSUITE='chocolate' BROWSER='firefox' $FOLDER/execute_compose_test.sh $@ &
 TESTSUITE='order-pdf' TEST_OS=centos $FOLDER/execute_compose_test.sh $@ &
 wait
